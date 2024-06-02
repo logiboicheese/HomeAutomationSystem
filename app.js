fe928b97794efa3b0e56f0ca979fc7827f5c03ea
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-06-03 01:11:02 +0200

    update variables

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 const deviceRoutes = require('./routes/deviceRoutes');
 
-const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 }); // TTL in seconds
-const mongoDB = process.env.MONGODB_URI;
+const appCache = new NodeCache({ stdTTL: 100, checkperiod: 120 }); // TTL in seconds
+const mongoDBUri = process.env.MONGODB_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
 app.use(express.json());
 
-const cacheMiddleware = (req, res, next) => {
-  const key = `${req.method}_${req.originalUrl}`;
-  const cachedResponse = myCache.get(key);
-  if (cachedResponse) {
+const cacheInterceptorMiddleware = (req, res, next) => {
+  const cacheKey = `${req.method}_${req.originalUrl}`;
+  const cachedResult = appCache.get(cacheKey);
+  if (cachedResult) {
     console.log('Cache hit');
-    return res.json(cachedResponse);
+    return res.json(cachedResult);
   }
   console.log('Cache miss');
-  const originalJson = res.json.bind(res);
+  const originalSendResponse = res.json.bind(res);
   res.json = (body) => {
-    myCache.set(key, body);
-    originalJson(body);
+    appCache.set(cacheKey, body);
+    originalSendResponse(body);
   };
   next();
 };
 
-app.use(cacheMiddleware);
+app.use(cacheInterceptorMiddleware);
 
 app.use('/devices', deviceRoutes);
 
@@ -39,8 +39,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the IoT Home Automation System' });
 });
 
-app.get('/heavy-calculation', (req, res) => {
-  performHeavyCalculation()
+app.get('/perform-intensive-task', (req, res) => {
+  calculateIntensiveTaskResult()
     .then(result => res.json(result))
     .catch(err => res.status(500).send(err.message));
 });
@@ -49,6 +49,6 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-function performHeavyCalculation() {
-  return Promise.resolve({ result: "Calculation result" });
+function calculateIntensiveTaskResult() {
+  return Promise.resolve({ result: "Intensive task calculation result" });
 }
